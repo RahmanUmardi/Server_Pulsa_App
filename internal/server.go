57d0cd1dba5886a4dce1e8e7d476f9aaed59bc3f
alfedohm10@gmail.com <alfedohm10@gmail.com>
2024-10-23 08:04:00 +0700

    ADD: establish db connection

---
+++ internal/server.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"database/sql"
+	"fmt"
+	"server-pulsa-app/config"
+
+	_ "github.com/lib/pq"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Server struct {
+	// usecase
+	engine *gin.Engine
+	host   string
+}
+
+func (s *Server) initRoute() {
+}
+
+func (s *Server) Run() {
+	s.initRoute()
+	if err := s.engine.Run(s.host); err != nil {
+		panic(fmt.Errorf("server not running on host %s, becauce error %v", s.host, err.Error()))
+	}
+}
+
+func NewServer() *Server {
+	cfg, _ := config.NewConfig()
+	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Name)
+	_, err := sql.Open(cfg.Driver, dsn)
+	if err != nil {
+		fmt.Println("connection error", err)
+	}
+
+	engine := gin.Default()
+	host := fmt.Sprintf(":%s", cfg.ApiPort)
+	return &Server{
+		engine: engine,
+		host:   host,
+	}
+}
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"server-pulsa-app/internal"
+)
 
+func main() {
+	fmt.Println("Server Pulsa App")
+	internal.NewServer().Run()
 }
