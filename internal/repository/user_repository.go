27fade70f4d6065d3b0447a58316ee275179ec9b
rifki <tiarsarifki.com>
2024-10-24 09:49:52 +0700

    add: logger in the merchant and product repository

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -20,16 +20,16 @@ type userRepository struct {
 
 func (u *userRepository) CreateUser(user entity.User) (entity.User, error) {
 	log := logger.GetLogger()
-	log.Info("Start creating a new user")
+	log.Infof("Starting to create a new user in the repository layer")
 
 	err := u.db.QueryRow(`INSERT INTO mst_user (username, password, role) VALUES ($1, $2, $3) RETURNING id_user`, user.Username, user.Password, user.Role).Scan(&user.Id_user)
 
 	if err != nil {
-		log.Error("Failed to create user: ", err)
+		log.Error("Failed to create the user: ", err)
 		return entity.User{}, err
 	}
 
-	log.Info("User created successfully: ", user)
+	log.Info("User has been created successfully: ", user)
 	return user, nil
 }
 
@@ -37,16 +37,16 @@ func (u *userRepository) GetUserByUsername(username string) (entity.User, error)
 	var user entity.User
 
 	log := logger.GetLogger()
-	log.Info("Start getting user by username")
+	log.Info("Starting to retrive a user by username in the repository layer")
 
 	err := u.db.QueryRow(`SELECT id_user, username, password, role FROM mst_user WHERE username = $1`, username).Scan(&user.Id_user, &user.Username, &user.Password, &user.Role)
 
 	if err != nil {
-		log.Error("Failed to get user: ", err)
+		log.Error("Failed to retrive the user: ", err)
 		return entity.User{}, err
 	}
 
-	log.Info("Get user by username successfully: ", user)
+	log.Info("Getting user by username was successfully: ", user)
 	return user, nil
 }
 
@@ -54,45 +54,45 @@ func (u *userRepository) GetUserByID(id string) (entity.User, error) {
 	var user entity.User
 
 	log := logger.GetLogger()
-	log.Info("Start getting user by id")
+	log.Info("Starting to retrive a user by id in the repository layer")
 
 	err := u.db.QueryRow(`SELECT id_user, username, password, role FROM mst_user WHERE id_user = $1`, id).Scan(&user.Id_user, &user.Username, &user.Password, &user.Role)
 
 	if err != nil {
-		log.Error("Failed to get user: ", err)
+		log.Error("Failed to retrive the user: ", err)
 		return entity.User{}, err
 	}
 
-	log.Info("Get user by id successfully: ", user)
+	log.Info("Getting user by id was successfully: ", user)
 	return user, nil
 
 }
 func (u *userRepository) UpdateUser(user entity.User) (entity.User, error) {
 	log := logger.GetLogger()
-	log.Info("Start updating user")
+	log.Info("Starting to update user in the repository layer")
 
 	_, err := u.db.Exec(`UPDATE mst_user SET username = $1, password = $2, role = $3 WHERE id_user = $4`, user.Username, user.Password, user.Role, user.Id_user)
 
 	if err != nil {
-		log.Error("Failed to update user: ", err)
+		log.Error("Failed to update the user: ", err)
 		return entity.User{}, err
 	}
 
-	log.Info("User updated successfully: ", user)
+	log.Info("User has been updated successfully: ", user)
 	return user, nil
 }
 func (u *userRepository) DeleteUser(id string) error {
 	log := logger.GetLogger()
-	log.Info("Start deleting user")
+	log.Info("Starting to delete user in the repository layer")
 
 	_, err := u.db.Exec(`DELETE FROM mst_user WHERE id_user = $1`, id)
 
 	if err != nil {
-		log.Error("Failed to delete user: ", err)
+		log.Error("Failed to delete the user: ", err)
 		return err
 	}
 
-	log.Info("User deleted successfully: ", id)
+	log.Info("User has been deleted successfully: ", id)
 	return nil
 }
 
