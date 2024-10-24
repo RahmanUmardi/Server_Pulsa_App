df3a6a1d21690627a5c9cfcce5b9224a95948a67
rifki <tiarsarifki.com>
2024-10-24 11:46:10 +0700

    add: logger in the handler layer

--- internal/handler/auth_handler.go
+++ internal/handler/auth_handler.go
@@ -1,13 +1,15 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 	"server-pulsa-app/internal/entity/dto"
+	"server-pulsa-app/internal/logger"
 	"server-pulsa-app/internal/usecase"
 
 	"github.com/gin-gonic/gin"
 )
+
+var logAuth = logger.GetLogger()
 
 type AuthController struct {
 	authUsecase usecase.AuthUseCase
@@ -16,31 +18,46 @@ type AuthController struct {
 
 func (a *AuthController) loginHandler(ctx *gin.Context) {
 	var payload dto.AuthRequestDto
+
+	logAuth.Info("Starting to login a user in the handler layer")
+
 	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		logAuth.Errorf("Invalid payload for login: %v", err)
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
+
+	logAuth.Info("Starting login")
 	token, err := a.authUsecase.Login(payload)
 	if err != nil {
+		logAuth.Error("Failed to authenticate user: ", err)
 		ctx.JSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
 		return
 	}
+
+	logAuth.Info("User has been authenticated successfully")
 	ctx.JSON(http.StatusOK, token)
 }
 
 func (a *AuthController) registerHandler(ctx *gin.Context) {
 	var payload dto.AuthRequestDto
 
+	logAuth.Info("Starting to register a new user in the handler layer")
 	if err := ctx.ShouldBindJSON(&payload); err != nil {
+		logAuth.Errorf("Invalid payload for register: %v", err)
 		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println("payload trigger: ", payload)
+
+	logAuth.Info("Starting to register new user")
 	user, err := a.authUsecase.Register(payload)
 	if err != nil {
+		logAuth.Error("Failed to register user: ", err)
 		ctx.JSON(http.StatusConflict, gin.H{"error": err.Error()})
 		return
 	}
+
+	logAuth.Info("User has been registered successfully")
 	ctx.JSON(http.StatusCreated, user)
 }
 
