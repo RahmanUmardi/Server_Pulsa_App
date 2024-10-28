0e5a53f51d8cff79b36adb22f68d364dfb27bf3a
rahmanumardi <rahmanumardi@gmail.com>
2024-10-28 19:43:45 +0700

    fixing:user updated terbaru

--- internal/usecase/user_usecase.go
+++ internal/usecase/user_usecase.go
@@ -88,23 +88,19 @@ func (u *userUsecase) FindUserByUsernamePassword(username, password string) (ent
 func (u *userUsecase) UpdateUser(user entity.User) (entity.User, error) {
 	u.log.Info("Starting to update a user in the usecase layer", nil)
 
-	oldUser, err := u.UserRepository.GetUserByID(user.Id_user)
+	_, err := u.UserRepository.GetUserByID(user.Id_user)
 	if err != nil {
 		u.log.Error("User ID %s not found: %v", user.Id_user)
 		return entity.User{}, fmt.Errorf("user ID %s not found", user.Id_user)
 	}
-	if user.Password != "" {
-		// u.log.Info("Starting to hash the password", nil)
-		// hash, err := bcrypt.GenerateFromPassword([]byte(oldUser.Password), bcrypt.DefaultCost)
-		// if err != nil {
-		// 	u.log.Error("Failed to hash password: ", err)
-		// 	return entity.User{}, fmt.Errorf("failed to hash password: %v", err)
-		// }
-		// oldUser.Password = string(hash)
-		user.Password = oldUser.Password
+	u.log.Info("Starting to hash the password", nil)
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	if err != nil {
+		u.log.Error("Failed to hash password: ", err)
+		return entity.User{}, fmt.Errorf("failed to hash password: %v", err)
 	}
+	user.Password = string(hash)
 
-	// user.Role = "employee"
 	updatedUser, err := u.UserRepository.UpdateUser(user)
 	if err != nil {
 		u.log.Error("Failed to update user: ", err)
